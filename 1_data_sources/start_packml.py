7eb406b3e48f62d6510d594ffbf8af478e7c7159
blanpa <pascalblansche@gmail.com>
2023-04-16 18:46:39 +0000

    fix

--- 1_data_sources/start_packml.py
+++ 1_data_sources/start_packml.py
@@ -1,7 +1,39 @@
 import paho.mqtt.client as mqtt
+import time
 
 broker_address="localhost" 
-#broker_address="iot.eclipse.org" #use external broker
 client = mqtt.Client("P1") #create new instance
 client.connect(broker_address) #connect to broker
-client.publish("house/main-light","OFF")#publish
+
+"""
+berlin
+    packaging
+        line_1
+        line_2
+        line_3
+    cnc
+        line_1
+        line_2
+        line_3
+"""
+# Site/Area/line_/xxx
+reset = "Command/Reset" 
+start = "Command/Start"
+
+berlin = [
+    "berlin/packaging/line_1/",
+    "berlin/packaging/line_2/",
+    "berlin/packaging/line_3/",
+    "berlin/cnc/line_1/",
+    "berlin/cnc/line_2/",
+    "berlin/cnc/line_3/",
+]
+for i in berlin:
+    topic_reset = str(i)+str(reset)
+    print(topic_reset)
+    client.publish(topic_reset,"1")#publish
+    time.sleep(1)
+    topic_start = str(i)+str(start)
+    print(topic_start)
+    client.publish(topic_start,"1")#publish
+    time.sleep(1)
--- 2_data_ingestion/README.md
+++ 2_data_ingestion/README.md
@@ -1,2 +1,6 @@
 Kafka
 https://docs.confluent.io/platform/current/platform-quickstart.html#step-1-download-and-start-cp
+
+
+set-up enviorment
+    sudo docker-compose -p "data_ingestion" up -d 
